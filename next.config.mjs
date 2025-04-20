439e8ca731d758b07b2ff423edc99cba7ea4971e
taimoor948 <taimoorhabib948@gmail.com>
2025-04-20 18:07:44 +0500

    final modi

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
+<<<<<<< HEAD
 const nextConfig = {
     reactStrictMode:false, 
     images:{
         domains:['img.freepik.com']
     }
 };
+=======
+const nextConfig = {};
+>>>>>>> 30e2bc9 (Initial commit from Create Next App)
 
 export default nextConfig;
